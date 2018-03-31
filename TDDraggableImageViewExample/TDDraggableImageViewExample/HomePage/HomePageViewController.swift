bff40083b44dac2ef2cb6119c08f5281528c82c2
TDClash <dtc74110@gmail.com>
2018-03-31 12:54:57 -0700

    fileView created and added home page view

--- TDDraggableImageViewExample/TDDraggableImageViewExample/HomePage/HomePageViewController.swift
+++ TDDraggableImageViewExample/TDDraggableImageViewExample/HomePage/HomePageViewController.swift
@@ -8,18 +8,27 @@
 
 import UIKit
 
+struct constants {
+    static let imageName: String = "FileIcon"
+    static let animationDuration: Double = 0.3
+}
+
 class HomePageViewController: UIViewController {
 
+    @IBOutlet weak var trashCanView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Create DraggableImageView instance and add to home page view as subview
+        let fileView = DraggableImageView(image: UIImage(named: constants.imageName)!,
+                                          origin: self.view.center,
+                                          destinationView: trashCanView,
+                                          parentView: self.view,
+                                          moveDuration: constants.animationDuration,
+                                          returnDuration: constants.animationDuration,
+                                          enableTapping: true)
+        self.view.addSubview(fileView)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
