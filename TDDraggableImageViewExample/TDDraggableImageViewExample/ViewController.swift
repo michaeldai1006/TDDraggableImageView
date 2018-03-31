ab31f8f749c7d6fd2ae0e9002a95223e6fd24475
TDClash <dtc74110@gmail.com>
2018-03-30 18:16:51 -0700

    Sample project created

---
+++ TDDraggableImageViewExample/TDDraggableImageViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDDraggableImageViewExample
+//
+//  Created by Michael Dai on 3/30/18.
+//  Copyright © 2018 Tiancheng Dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
